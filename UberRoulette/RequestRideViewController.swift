9194cf642635690ec3efb50902a34da27045fdf2
Sam Raudabaugh <ser266@cornell.edu>
2015-09-19 20:37:30 -0400

    DRY the sliders.

--- UberRoulette/RequestRideViewController.swift
+++ UberRoulette/RequestRideViewController.swift
@@ -16,23 +16,18 @@ class RequestRideViewController: UIViewController, ASValueTrackingSliderDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        costSlider.maximumValue = 200.0
-        costSlider.minimumValue = 1.0
-        costSlider.popUpViewCornerRadius = 12.0
-        costSlider.setMaxFractionDigitsDisplayed(2)
-        costSlider.popUpViewColor = UIColor(hue: 0.55, saturation: 0.8, brightness: 0.9, alpha: 0.7)
-        costSlider.font = UIFont(name: "GillSans-Bold", size: 22)
-        costSlider.textColor = UIColor(hue: 0.55, saturation: 1.0, brightness: 0.5, alpha: 1)
-        costSlider.dataSource = self
-        
-        distanceSlider.maximumValue = 50.0
-        distanceSlider.minimumValue = 1.0
-        distanceSlider.popUpViewCornerRadius = 12.0
-        distanceSlider.setMaxFractionDigitsDisplayed(1)
-        distanceSlider.popUpViewColor = UIColor(hue: 0.55, saturation: 0.8, brightness: 0.9, alpha: 0.7)
-        distanceSlider.font = UIFont(name: "GillSans-Bold", size: 22)
-        distanceSlider.textColor = UIColor(hue: 0.55, saturation: 1.0, brightness: 0.5, alpha: 1)
-        distanceSlider.dataSource = self
+        setup(costSlider, max: 200.0)
+        setup(distanceSlider, max: 50.0)
+    }
+    
+    func setup(slider: ASValueTrackingSlider, max: Float) {
+        slider.maximumValue = max
+        slider.minimumValue = 1.0
+        slider.popUpViewCornerRadius = 12.0
+        slider.popUpViewColor = UIColor(hue: 0.55, saturation: 0.8, brightness: 0.9, alpha: 0.7)
+        slider.font = UIFont(name: "GillSans-Bold", size: 22)
+        slider.textColor = UIColor(hue: 0.55, saturation: 1.0, brightness: 0.5, alpha: 1)
+        slider.dataSource = self
     }
 
     func slider(slider: ASValueTrackingSlider!, stringForValue value: Float) -> String! {
